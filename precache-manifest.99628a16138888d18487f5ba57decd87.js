a0924d25a6a60d590ebae75552b5e6b48e940b29
Olha Savka <tokarchykolia@gmail.com>
2020-08-04 23:56:05 +0300

    Updates

---
+++ precache-manifest.99628a16138888d18487f5ba57decd87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc6955418f1779d096f6eede6dd11f1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "dedad3e62275d767fc56",
+    "url": "/my-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a7a9c743218e7c43eb55",
+    "url": "/my-app/static/css/main.60158b1c.chunk.css"
+  },
+  {
+    "revision": "dedad3e62275d767fc56",
+    "url": "/my-app/static/js/2.f9a3befd.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/my-app/static/js/2.f9a3befd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a9c743218e7c43eb55",
+    "url": "/my-app/static/js/main.2e3019bb.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
